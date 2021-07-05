a91f7ae1e77697ced8729a39f5a9e7ba0d5dfe6f
cherylaitt <cherylai481@gmail.com>
2021-07-05 17:52:51 +0800

    initial

--- views/test2.php
+++ views/test2.php
@@ -221,13 +221,11 @@
             </div>
         </div>
         <div class="td" style="justify-content: center;">
-            <button>
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                 class="bi bi-three-dots-vertical" viewBox="0 0 16 16">
                 <path
                     d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z" />
             </svg>
-            </button>
         </div>
     </div>
     <div class="tr">
